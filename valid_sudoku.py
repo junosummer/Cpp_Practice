7e6db72bbcea318dc36c8b99bf53e0de4f7f06ab
junosummer <31232873+junosummer@users.noreply.github.com>
2018-02-03 14:53:52 +0000

    Update valid_sudoku.py
    Fix some typos

--- valid_sudoku.py
+++ valid_sudoku.py
@@ -1,10 +1,10 @@
-// Leetcode Problem 36 Valid Sudoku
+# Leetcode Problem 36 Valid Sudoku
 
-// Solution in C++ may be uploaded later.
+# Solution in C++ may be uploaded later.
 
-// Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
-// The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
-// A valid Sudoku board (partially filled) is not necessarily solvable. Only the filled cells need to be validated.
+# Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
+# The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+# A valid Sudoku board (partially filled) is not necessarily solvable. Only the filled cells need to be validated.
 
 class Solution:
     def isValidSudoku(self, board):
